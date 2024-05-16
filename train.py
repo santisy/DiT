7efd2b763c252187c01ab21ebd7b6dfd5058e450
Anonymous Authors <N/A>
2024-05-15 18:06:10 -0700

    Add max_grad_norm to 1.0

--- train.py
+++ train.py
@@ -341,7 +341,7 @@ def main(args):
             opt.zero_grad()
             scaler.scale(loss).backward()
             scaler.unscale_(opt)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             scaler.step(opt)
             scaler.update()
             update_ema(ema, model.module)
