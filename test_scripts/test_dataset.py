b447866aa5b3bee6302be0938f9111c65e62ce36
Anonymous Authors <N/A>
2024-05-20 19:27:53 -0700

    Test

--- test_scripts/test_dataset.py
+++ test_scripts/test_dataset.py
@@ -1,11 +1,15 @@
 import sys
+from data_extensions import load_utils
 sys.path.insert(0, ".")
 from data.ofalg_dataset import OFLAGDataset
 
 dataset = OFLAGDataset("datasets/shapenet_airplane")
-x0, x1, x2, _, _, _, _ = dataset[0]
+x0, x1, _, _, _ = dataset[0]
 print(x0.shape)
 print(x1.shape)
-print(x2.shape)
-import pdb; pdb.set_trace()
 print("Checking")
+
+x0 = dataset.denormalize(x0, 0)
+x1 = dataset.denormalize(x1, 1)
+
+load_utils.dump_to_bin("test.bin", x0, x1, dataset.octree_root_num)
