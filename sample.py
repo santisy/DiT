d0e2289eba0e33c1f46dbfdb608068a722681fbf
Anonymous Authors <N/A>
2024-05-28 19:53:09 -0700

    fix the sample input

--- sample.py
+++ sample.py
@@ -41,7 +41,7 @@ def main(args):
     torch.set_grad_enabled(False)
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    in_ch = dataset.get_level_vec_len(2)
+    in_ch = dataset.get_level_vec_len(1)
     m = int(math.floor(math.pow(in_ch, 1 / 3.0)))
     sibling_num = config.model.get("sibling_num", 2)
 
